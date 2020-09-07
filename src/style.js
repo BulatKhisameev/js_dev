ea2655e20c8b034d927783877db36df6b30d5ce0
Bulat Khisameev <khisameevbi@gmail.com>
2020-09-07 14:41:43 +0300

    задание пятого урока

--- src/style.js
+++ src/style.js
@@ -1,30 +1,81 @@
 'use strict';
 
-// обращаемся к пользователю с вопросами и записываем полученные ответы
-let money = Number(prompt('Ваш месячный доход?', 56000));
+let isNumber = function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+    // обращаемся к пользователю с вопросами и записываем полученные ответы
+let money;
 let income = Number(prompt('Какой у вас дополнительный доход?', 25000))
+let mission = 10000000;
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 let deposit = confirm('Есть ли у вас депозит в банке?');
-let expenses1 = prompt('Введите обязательную статью расходов?', 'ипотека');
-let expenses2 = prompt('Введите обязательную статью расходов?', 'еда');
-let amount1 = Number(prompt(`Во сколько обойдется ${expenses1}?`, 30000));
-let amount2 = Number(prompt(`Во сколько обойдется ${expenses2}?`, 8000));
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
-console.log(addExpenses.split(','));
-//рассчитываем месячный бюджет
-let budgetMonth = (money - amount1 - amount2);
-console.log(budgetMonth);
+
+let start = function() {
+    do {
+        money = prompt('Ваш месячный доход?');
+    }
+    while (!isNumber(money))
+};
+
+start();
+
+
+let showTypeof = function(item) {
+    console.log(typeof item);
+};
+showTypeof(money);
+showTypeof(income);
+showTypeof(deposit);
+
+let expenses = [];
+
+console.log(addExpenses.toLowerCase().split(','));
+
+//
+let getExpensesMonth = function() {
+    let sum;
+    do {
+        for (let i = 0; i < 1; i++) {
+
+            expenses[i] = prompt('Введите обязательную статью расходов?');
+
+            sum = prompt(`Во сколько обойдется?`);
+        }
+    }
+    while (!isNumber(sum));
+
+    console.log(expenses);
+    return sum;
+};
+
+
+let expensesAmount = getExpensesMonth();
+
+console.log('Расходы за месяц: ' + expensesAmount);
+
+let getAccumulatedMonth = function() {
+    return money - expensesAmount;
+};
+
+let accumulatedMonth = getAccumulatedMonth();
+
+let getTargetMonth = function() {
+    return mission / accumulatedMonth
+};
+
+console.log(accumulatedMonth);
 
 //рассчитываем срок достижения цели
-let mission = 10000000;
-let period = Math.ceil(mission / budgetMonth);
-console.log(`Цель будет достигнета через: ${period} месяца`);
+if (getTargetMonth() >= 0) {
+    let period = Math.ceil(mission / accumulatedMonth);
+    console.log(`Цель будет достигнета через: ${period} месяца`);
+} else {
+    console.log(`Цель не будет достигнута`);
+}
 
-//рассчитываем дневной бюджет
-let budgetDay = Math.floor(budgetMonth / 30);
+// //рассчитываем дневной бюджет
+let budgetDay = Math.floor(accumulatedMonth / 30);
 console.log(`Бюджет на день: ${budgetDay}`);
 
 // сообщаем пользователю его финансовое состояние
@@ -33,48 +84,3 @@ let getStatusIncome = (budgetDay < 0) ? 'Что-то пошло не так' :
     (budgetDay >= 600) ? 'У вас средний уровень дохода' :
     'К сожалению у вас уровень дохода ниже среднего!';
 console.log(getStatusIncome);
-
-
-
-// задания четвертого урока
-
-//создаем функцию для рассчета суммы обязательных платежей
-let allExpensesOfMonth = 0;
-
-function getExpensesMonth(data1, data2) {
-    allExpensesOfMonth = data1 + data2;
-}
-getExpensesMonth(amount1, amount2);
-console.log(allExpensesOfMonth);
-
-//задаем функцию рассчета накполений за месяц
-
-let accumulatedMonth = 0;
-
-function getAccumulatedMonth(data1, data2) {
-    accumulatedMonth = data1 - data2;
-}
-getAccumulatedMonth(money, allExpensesOfMonth);
-console.log(accumulatedMonth);
-
-//задаем функцию подсчета периода достижения цели
-
-let timeTarget = 0;
-
-function getTargetMont(data1, data2) {
-    timeTarget = data1 / data2;
-}
-
-getTargetMont(money, accumulatedMonth);
-console.log(Math.ceil(timeTarget));
-
-//удаляем переменную budgetMonth
-
-delete window['budgetMonth'];
-alert(budgetMonth);
-
-//рассчитываем budgetDay на основе accumulatedMonth
-
-budgetDay = accumulatedMonth / 30;
-
-// рассчитываем общий доход за месяц
